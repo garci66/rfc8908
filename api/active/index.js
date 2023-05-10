80f2fb99cf4f754a057d549f04cd39516cb69703
Diego Garcia del Rio <garci66@gmail.com>
2023-05-10 11:55:59 -0300

    Update index.js

--- api/active/index.js
+++ api/active/index.js
@@ -4,11 +4,12 @@ export const config = {
   
 export default async function (req) {
   const myUrl = new URL(req.url).searchParams;
-  var myResponse={'captive':false, "user-portal-url":"https://activelearning.school-wifi.com/captive-portal" }
+  var myResponseObj={'captive':false, "user-portal-url":"https://activelearning.school-wifi.com/captive-portal" }
   const rfc8908 = myUrl.get('rfc8908')
   if (rfc8908){
-    myResponse['user-portal-url']+=`?rfc8908={$rfc8908}`
+    myResponseObj['user-portal-url']+=`?rfc8908={$rfc8908}`
   }
-  return myResponse
+  
+  return new Response(myResponseObj)
 }
   
