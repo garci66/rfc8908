fe75a18d2f210778feea10b9252d79d9e6534072
Diego Garcia del Rio <garci66@gmail.com>
2023-05-10 14:15:05 -0300

    Update index.js

--- api/active/index.js
+++ api/active/index.js
@@ -7,7 +7,7 @@ export default async function (req) {
   var myResponseObj={'captive':true, "user-portal-url":"https://activelearning.school-wifi.com/captive-portal" }
   const rfc8908 = myUrl.get('rfc8908')
   if (rfc8908){
-    myResponseObj['user-portal-url']+=`?rfc8908=${rfc8908}`
+    myResponseObj['user-portal-url']+=`?ref=${rfc8908}&vercel`
   }
   
   return new Response(JSON.stringify(myResponseObj),{status:200,  headers: {'Content-Type': 'application/json'}})
