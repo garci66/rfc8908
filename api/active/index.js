2767296b35f91144df6ac11589470a4f41d5b50e
Diego Garcia del Rio <garci66@gmail.com>
2023-05-10 14:49:08 -0300

    only set captive=true

--- api/active/index.js
+++ api/active/index.js
@@ -3,13 +3,7 @@ export const config = {
   };
   
 export default async function (req) {
-  const myUrl = new URL(req.url).searchParams;
-  var myResponseObj={'captive':true, "user-portal-url":"https://activelearning.school-wifi.com/captive-portal" }
-  const rfc8908 = myUrl.get('rfc8908')
-  if (rfc8908){
-    myResponseObj['user-portal-url']+=`?ref=${rfc8908}&vercel`
-  }
-  
+  var myResponseObj={'captive':true }
   return new Response(JSON.stringify(myResponseObj),{status:200,  headers: {'Content-Type': 'application/json'}})
 }
   
